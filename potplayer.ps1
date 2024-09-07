32eacedf3b3df008a4208d81ade23b1ee4c7529b
ramondsq <57244337+ramondsq@users.noreply.github.com>
2024-09-07 22:54:15 +0800

    Update potplayer.ps1
    a little optimization

--- potplayer.ps1
+++ potplayer.ps1
@@ -1,6 +1,5 @@
-Add-Type -Assembly System.Web
-$path=$args[0]
-$path=$path -replace "potplayer://" , ""
-$path= [System.Web.HttpUtility]::UrlDecode($path)
+Add-Type -AssemblyName "System.Web"
+$path = $args[0] -replace "potplayer://", ""
+$path = [System.Web.HttpUtility]::UrlDecode($path)
 echo $path
 & "C:\\Program Files\\DAUM\\PotPlayer\\PotPlayerMini64.exe" $path
