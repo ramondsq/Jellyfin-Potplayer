d727c1501917dfafea830f5698e983c9d6bf38c0
ramondsq <57244337+ramondsq@users.noreply.github.com>
2024-10-26 22:05:17 +0800

    Update potplayer.ps1
    bug fix

--- potplayer.ps1
+++ potplayer.ps1
@@ -1,5 +1,7 @@
 Add-Type -AssemblyName "System.Web"
 $path = $args[0] -replace "potplayer://", ""
 $path = [System.Web.HttpUtility]::UrlDecode($path)
+# 将开头的 X/ 替换为 X:/
+$path = $path -replace "^([A-Za-z])/", '$1:/'
 echo $path
 & "C:\\Program Files\\DAUM\\PotPlayer\\PotPlayerMini64.exe" $path
